68d0b04992e612aacf61de816f3e59ff3e2acfc1
MuhRifa2024 <muhamad.rifaldi174@gmail.com>
2025-11-01 10:28:57 +0700

    Menambahkan Tugas P3_2

---
+++ pertemuan03/Tugas/P3_2_714240044/P3_2_714240044/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_2_714240044
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("=== MENU PERSEGI PANJANG ===");
+                Console.WriteLine("1. Hitung Luas");
+                Console.WriteLine("2. Hitung Keliling");
+                Console.WriteLine("3. Keluar");
+                Console.Write("Pilih menu (1-3): ");
+                int pilihan = int.Parse(Console.ReadLine());
+
+                if (pilihan == 1)
+                {
+                    Console.Write("\nMasukan Panjang: ");
+                    double panjang = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Masukan Lebar: ");
+                    double lebar = Convert.ToDouble(Console.ReadLine());
+                    double luas = panjang * lebar;
+                    Console.WriteLine("Luas Persegi Panjang: " + luas);
+                }
+                else if (pilihan == 2)
+                {
+                    Console.Write("\nMasukan Panjang: ");
+                    double panjang = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Masukan Lebar: ");
+                    double lebar = Convert.ToDouble(Console.ReadLine());
+                    double keliling = 2 * (panjang + lebar);
+                    Console.WriteLine("Keliling Persegi Panjang: " + keliling);
+                }
+                else if (pilihan == 3)
+                {
+                    Console.WriteLine("Program selesai");
+                    Console.WriteLine("Terimakasih!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Menu tidak tersedia. Silakan pilih menu yang valid.");
+                }
+                Console.Write("\nIngin mengulang Kembali (Y/T)? ");
+            }
+            while (Console.ReadLine() == "Y");
+        }
+    }
+}
