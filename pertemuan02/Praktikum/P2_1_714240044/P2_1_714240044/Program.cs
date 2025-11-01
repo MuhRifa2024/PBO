23d4b60f5cb2407179ef11d7fdac6541dc665482
MuhRifa2024 <muhamad.rifaldi174@gmail.com>
2025-11-01 12:29:21 +0700

    Tambah

---
+++ pertemuan02/Praktikum/P2_1_714240044/P2_1_714240044/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_714240044
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Soal ===");
+            Console.WriteLine("Masukan angka pertama= ");
+            double angka1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Masukan angka kedua= ");
+            double angka2 = Convert.ToDouble(Console.ReadLine());
+
+             var hasil = angka1 + angka2;
+             var hasil2 = angka1 - angka2;
+             var hasil3 = angka1 * angka2;
+             var hasil4 = angka1 / angka2;
+            Console.WriteLine($"{angka1} + {angka2} = {hasil}");
+            Console.WriteLine($"{angka1} - {angka2} = {hasil2}");
+            Console.WriteLine($"{angka1} x {angka2} = {hasil3}");
+            Console.WriteLine($"{angka1} : {angka2} = {hasil4}");
+
+
+        }
+    }
+}
